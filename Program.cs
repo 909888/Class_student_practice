c549402f5d16e299a1c6708ce72e01751f0cff20
ed.lopez1905@gmail.com <ed.lopez1905@gmail.com>
2018-10-18 19:34:17 -0700

    upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practiceTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Student student1 = new Stude nt("Jim", "Business", 2.8);
+            Student student2 = new Student("Pam", "Art", 3.6);
+
+            Console.WriteLine(student1.hasHonors());
+            Console.WriteLine(student2.hasHonors());
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_practiceTwo
+{
+    class Student
+    {
+
+        public string name;
+        public string major;
+        public double gpa;
+
+        //constructor:
+        public Student(string aName, string aMajor, double aGpa)
+        {
+            name = aName;
+            major = aMajor;
+            gpa = aGpa;
+
+        }
+
+
+        //method to check if student is honor student
+        public bool hasHonors()
+        {
+            if(gpa >= 3.5)
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+
+    }
+}
